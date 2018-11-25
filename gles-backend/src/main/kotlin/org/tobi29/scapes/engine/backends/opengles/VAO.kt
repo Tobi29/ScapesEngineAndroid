1c209d1a8075e7eb8fdcdf1a5bcc3135f86a93e4
Git out :V <tobi291019@gmail.com>
2018-11-25 02:23:58 +0100

    Update gles-backend code

--- gles-backend/src/main/kotlin/org/tobi29/scapes/engine/backends/opengles/VAO.kt
+++ gles-backend/src/main/kotlin/org/tobi29/scapes/engine/backends/opengles/VAO.kt
@@ -91,21 +91,21 @@ internal abstract class VAO(protected val glh: GLESHandle) : Model {
         if (uniformLocation != GLUniform_EMPTY) {
             glh.glUniformMatrix4fv(
                 uniformLocation, false,
-                matrix.modelView().values
+                matrix.modelViewMatrix.array.array
             )
         }
         uniformLocation = shader.uniformLocation(1)
         if (uniformLocation != GLUniform_EMPTY) {
             glh.glUniformMatrix4fv(
                 uniformLocation, false,
-                matrix.modelViewProjection().values
+                matrix.modelViewProjectionMatrix.array.array
             )
         }
         uniformLocation = shader.uniformLocation(2)
         if (uniformLocation != GLUniform_EMPTY) {
             glh.glUniformMatrix3fv(
                 uniformLocation, false,
-                matrix.normal().values
+                matrix.normalMatrix.array.array
             )
         }
     }
